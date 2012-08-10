187aeeac5f025a913543eda7439845f652ad77d1
Alex Bosworth <alex.bosworth@gmail.com>
2012-08-10 11:13:47 +0800

    init

---
+++ web.js
@@ -0,0 +1,17 @@
+var express = require('express'),
+    NODE_MEMORY_LIMIT = 475,
+    LOG_FORMAT = ':method :url :status - :response-time ms - :user-agent';
+
+var app = express(),
+    port = process.env.PORT || 11115;
+
+app.listen(port, function(){ console.log("Listening on " + port); });
+
+app.use(express.logger(LOG_FORMAT));
+app.use(express.static(__dirname + '/public'));
+app.use(express.bodyParser());
+
+setInterval(function() {
+    if (Math.round(process.memoryUsage().rss / 1024 / 1024) > NODE_MEMORY_LIMIT)
+        process.exit();
+}, 5000);
